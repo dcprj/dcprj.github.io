fe7f1dfa04e424e72942e382116aa052de8bb9fd
Dario Pinheiro <dpinheiro@gmail.com>
2020-11-29 12:10:14 -0300

    Exercicio - Pergunte nome e sobrenome e exiba na tela

---
+++ Semana-02/Pergunte-nome-e-sobrenome-e-exiba-na-tela/script.js
@@ -0,0 +1,6 @@
+function Questionario() {
+    var sobreNome = prompt("Qual o seu sobrenome?");
+    var nome = prompt("Qual o seu nome?");      
+    alert(nome + " " + sobreNome +
+    ",\nObrigado pelas respostas.");
+}
