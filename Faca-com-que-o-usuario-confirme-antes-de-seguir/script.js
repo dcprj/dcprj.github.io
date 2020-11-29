71a26dc2115bb4ea0dba8d0689862e7023d1d035
Dario Pinheiro <dpinheiro@gmail.com>
2020-11-29 01:43:50 -0300

    Remocao dos artifícios para depuração do bug

--- Faca-com-que-o-usuario-confirme-antes-de-seguir/script.js
+++ Faca-com-que-o-usuario-confirme-antes-de-seguir/script.js
@@ -2,10 +2,8 @@ var receita = document.getElementById('receita');
 var alternativo = document.getElementById('alternativo');
 
 function TesteDeCoragem() {
-    Limpa(); // tava um console.log aqui, por isso não tava chamando
-    setTimeout(() => Segredo(), 1); /* botei com 1 milissegundo de atraso pra
-    só acontecer depois que limpou a tela, se não a tela de confirmação 
-    acaba acontecendo antes de atualizar a tela */
+    Limpa();
+    setTimeout(() => Segredo(), 1);
 }
 
 function Segredo() {
@@ -25,5 +23,4 @@ function Segredo() {
 function Limpa() {
     receita.style.display = 'none';
     alternativo.style.display = 'none';
-    return 'limpo';
 }
